0b0aa7f8cfcf4b40b03bc928dc06a675ce205eab
teovonkesselstatt <54458556+teovonkesselstatt@users.noreply.github.com>
2022-08-17 16:33:51 -0300

    Add files via upload

--- run_dashboard.py
+++ run_dashboard.py
@@ -1,7 +1,7 @@
 from webapp import app
+from streamlit import cli as stcli
 import streamlit as st
 import sys
-from streamlit import cli as stcli
 
 if __name__ == "__main__":
     if st._is_running_with_streamlit:
